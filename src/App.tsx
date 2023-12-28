aa8b1fa1dd0e7f5fa2e8a0da228e7c320dce9e21
irinarinch <you@example.com>
2023-12-28 11:11:41 +0800

    changed app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-// import Notes from "./components/crud/Notes";
-// import Watches from "./components/watches/Watches";
+import Notes from "./components/crud/Notes";
+import Watches from "./components/watches/Watches";
 
 function App() {
   return (
     <>
       <h1>Жизненный цикл и работа с HTTP</h1>
-      {/* <div className="task">
+      <div className="task">
         <h4>Задача 1. Мировые часы</h4>
         <Watches />
       </div>
       <div className="task">
         <h4>Задача 2. CRUD</h4>
         <Notes />
-      </div> */}
+      </div>
     </>
   );
 }
